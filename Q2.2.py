e949a96e3d06329eb7c04f6950af89aa5bb8ed09
Pieter Gort <p.c.gort@student.tue.nl>
2023-12-11 19:58:28 +0000

    made branch for question2.2b

--- Q2.2.py
+++ Q2.2.py
@@ -50,36 +50,37 @@ class FeedforwardNetwork(tf.keras.Model):
         x = self.dense2(x)
         return self.output_layer(x)
     
-best_batch = 0
+learning_rates = [0.001, 0.01, 0.1, 1]
+batch_size = 16
+best_lr = 0
 best_accuracy = 0
 history_dict = {}
 
-# Train and evaluate the model using different batch sizes
-for batch_size in batch_sizes:
-    start_time = time.time()
 
-    # Convert the data and labels into TensorFlow datasets
-    train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(buffer_size=1024).batch(batch_size)
-    dev_dataset = tf.data.Dataset.from_tensor_slices((dev_images, dev_labels)).batch(batch_size)
-    test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(batch_size)
+# Convert the data and labels into TensorFlow datasets
+train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(buffer_size=1024).batch(batch_size)
+dev_dataset = tf.data.Dataset.from_tensor_slices((dev_images, dev_labels)).batch(batch_size)
+test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(batch_size)
 
-    # Initialize the model
-    model = FeedforwardNetwork(hidden_size=hidden_size)
+# Initialize the model
+model = FeedforwardNetwork(hidden_size=hidden_size)
 
+for lr in learning_rates:
+    begin_time = time.time()
     # Compile the model
-    model.compile(optimizer=SGD(learning_rate=0.1),
+    model.compile(optimizer=SGD(learning_rate=lr),
                 loss=SparseCategoricalCrossentropy(),
                 metrics=['accuracy'])
 
     # Train the model
     history = model.fit(train_dataset, epochs=epochs, batch_size=batch_size, validation_data=dev_dataset)
 
-    history_dict[batch_size] = history.history
+    history_dict[lr] = history.history
 
     val_accuracy = max(history.history['val_accuracy'])
     if val_accuracy > best_accuracy:
         best_accuracy = val_accuracy
-        best_batch = batch_size
+        best_lr = lr
         best_model = model
 
     # Time taken per batch size
@@ -89,7 +90,7 @@ for batch_size in batch_sizes:
     plt.subplot(1, 2, 1)
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
-    plt.title(f'Model accuracy for batch size {batch_size}')
+    plt.title(f'Model accuracy for learning rate {lr}')
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Val'], loc='upper left')
@@ -97,17 +98,19 @@ for batch_size in batch_sizes:
     plt.subplot(1, 2, 2)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
-    plt.title(f'Model loss for batch size {batch_size}')
+    plt.title(f'Model loss for learning rate {lr}')
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Val'], loc='upper left')
     plt.show()
+
+    print(f'Time taken for learning rate {lr}: {end_time - begin_time} seconds')
     
-print(f'best learning rate: {best_batch} with validation accuracy: {best_accuracy}')
+print(f'best learning rate: {best_lr} with validation accuracy: {best_accuracy}')
     
 # Evaluate the model
-test_loss, test_accuracy = best_model.evaluate(test_dataset, batch_size=best_batch)
-print(f'Test accuracy with batch size {best_batch}: {best_accuracy}')
+test_loss, test_accuracy = best_model.evaluate(test_dataset, learning_rate = best_lr)
+print(f'Test accuracy with learning rate {best_lr}: {test_accuracy}')
 
 
     

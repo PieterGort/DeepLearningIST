2f2706eb42f8ff5f264c2349a109ba7fc6a89e35
Pieter Gort <p.c.gort@student.tue.nl>
2023-12-15 21:53:37 +0000

    final changes on weights and biases

--- Q3test.py
+++ Q3test.py
@@ -5,22 +5,17 @@ hidden_size = 2
 n_features = 3
 n_classes = 1
 
-
 class MLP(object):
     def __init__(self, A, B, D, K):
-
-        # ensure that the rules of the game are adhered to
         assert -D < A <= B < D, "A, B, D must satisfy -D < A <= B < D"
-
-        self.W1 = np.array([[1, 1], [-1, -1]])
+        self.W1 = np.array([[1, 1], [-1,-1]])
         self.b1 = np.array([[-A], [B]])
-
         self.W2 = np.array([[1, 1]])
-        self.b2 = -1 # or -2
-    
+        self.b2 = -2
+
     def sign(self, Z):
         return np.where(Z >= 0, 1, -1)
-    
+
     def forward(self, X):
         Z1 = self.W1.dot(X) + self.b1
         h1 = self.sign(Z1)
@@ -41,8 +36,6 @@ def main():
     K = 2
 
     mlp = MLP(A, B, D, K)
-    # let the input vector be any random vector of size Dx1 with entries -1 or 1
-    # make the vector [-1, 1]^T
     x1 = np.array([[1], [1]])
     x2 = np.array([[1], [-1]])
     x3 = np.array([[-1], [1]])
@@ -52,6 +45,7 @@ def main():
     output2 = mlp.forward(x2)
     output3 = mlp.forward(x3)
     output4 = mlp.forward(x4)
+
     print(" ")
     print("Output for x1: ", output1)
     print("Output for x2: ", output2)
@@ -71,9 +65,5 @@ def main():
     print("")
     print("Bias b2: ", mlp.b2)
 
-    
 if __name__ == '__main__':
     main()
-
-
-

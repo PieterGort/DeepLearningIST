0f38d1a225d3dcf5952473152d0bd4622b3a2711
pieter gort <p.c.gort@student.tue.nl>
2023-12-21 12:43:11 +0000

    initial commit hw2

--- README.md
+++ README.md
@@ -1,25 +1,23 @@
-# DeepLearningIST
-This readme contains everything you need to complete Homework 1. It contains
-the following files:
+# Homework 2 - Skeleton Code
 
-utils.py
-- used to create the datasets for the classification task at hand.
+This archive contains everything you need to complete Homework 2. It contains the following files:
 
-download_octmnist.py
-- allows you to download the OCTMNIST dataset to a compressed .npz file,
-  which hw1-q2.py and hw1-q3.py can load.
+## download_octmnist.py
 
-hw1-q1.py
-- contains the code for Question 1, which covers classification with
-  the perception, logistic regression, and the multi-layer perceptron, with
-  implementation in numpy. 
+Allows you to download the OCTMNIST dataset to a compressed `.npz` file, which `hw2-q2.py` can load.
 
-hw1-q2.py
-- contains the code for Question 2, which covers classification with
-  logistic regression and the multi-layer perceptron, with implementation in
-  Pytorch.
+## utils.py
 
-hw1-q3.py
-- contains some code to double check whether the network structure for question 3 could mimick the given function in the question properly. It just passes the vectors for a certain A,B and D through a forward pass function for the specified network.
+Utility functions for `hw2-q2.py`
 
+## hw2-q2.py
 
+Contains skeleton code for Question 2, which covers image classification using CNNs, with implementation in torch.
+
+## hw2-q3.ipynb
+
+Contains skeleton code for Question 3, which covers a sequence-to-sequence task using RNNs and attention, with implementation in torch. We suggest using *google colab* or *kaggle* to run the notebook with GPU accelerators.
+
+## how-to-use-kaggle-tutorial.md
+
+A simple tutorial on how to run the notebook `hw2-q3.ipynb` in kaggle.
--- utils.py
+++ utils.py
@@ -2,7 +2,12 @@ import os
 import random
 
 import numpy as np
+import pandas as pd
 import torch
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+import time
+from PIL import Image
 
 
 def configure_seed(seed):
@@ -15,16 +20,18 @@ def configure_seed(seed):
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
 
-def load_oct_data(bias=False, eq_test_dist=False, root="."):
+def load_oct_data(bias=False, eq_test_dist=False):
     """
-    Loads the preprocessed, featurized octmnist dataset, optionally adding a bias feature.
+    Loads the preprocessed, featurized fashion-mnist dataset from
+    Fashion-MNIST.npz, optionally adding a bias feature.
     """
-    path = os.path.join(root, "octmnist.npz")
-    data = np.load(path)
+    data = np.load('octmnist.npz')
     train_X = data["train_images"].reshape([data["train_images"].shape[0], -1])/256
     dev_X = data["val_images"].reshape([data["val_images"].shape[0], -1])/256
     test_X = data["test_images"].reshape([data["test_images"].shape[0], -1])/256
-
+    
+    train_y = np.asarray(data["train_labels"]).squeeze()
+    val_y = np.asarray(data["val_labels"]).squeeze()
     test_y = np.asarray(data["test_labels"]).squeeze()
     if not eq_test_dist:
         test_y_class0 = test_y[test_y == 0][0:182] #182
@@ -49,7 +56,7 @@ def load_oct_data(bias=False, eq_test_dist=False, root="."):
         test_X = np.hstack((test_X, np.ones((test_X.shape[0], 1))))
     return {"train": (train_X, np.asarray(data["train_labels"]).squeeze()),
             "dev": (dev_X, np.asarray(data["val_labels"]).squeeze()),
-            "test": (test_X, test_y)}
+            "test": (test_X, test_y)}#np.asarray(data["test_labels"]).squeeze())}
 
 class ClassificationDataset(torch.utils.data.Dataset):
 
